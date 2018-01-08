6a7e97534be40042c9eb048c7d539233024eb8e3
Denis Bartashevich <bartashevich@ua.pt>
2018-01-08 21:48:26 +0000

    plot2, working buttons + zoom + different color range for each map

--- js/plot2.js
+++ js/plot2.js
@@ -3,6 +3,7 @@
 //Width and height
 var w = 800;
 var h = 600;
+var plot2_type = "total_occurrences";
 
 var format = d3.format(",");
 
@@ -11,56 +12,27 @@ var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
     .html(function(d) {
-        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Fire occurrences: </strong><span class='details'>" + format(d.population) +"</span>";
-    })
+        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Fire occurrences: </strong><span class='details'>" + format(d.value) +"</span>";
+    });
 
 var margin = {top: 0, right: 0, bottom: 0, left: 0},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var color = d3.scaleThreshold()
-    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
-
 // http://www.perbang.dk/rgbgradient/
 function customColor(value) {
+    var maxVal = {'total_occurrences': 10000, 'area_occurrences': 0.04, 'bright_ti4_high': 500, 'bright_ti4_avg': 350};
     var colors = ['#CAE500','#C8CF00','#C7B900','#C6A300','#C58E00','#C47800','#C36200','#C24D00','#C13700','#C02100','#BF0C00'];
 
     if(isNaN(value)){
         return '#000000';
     }
-    else if(value < 1000){
-        return colors[0];
-    }
-    else if(value < 2000){
-        return colors[1];
-    }
-    else if(value < 3000){
-        return colors[2];
-    }
-    else if(value < 4000){
-        return colors[3];
-    }
-    else if(value < 5000){
-        return colors[4];
-    }
-    else if(value < 6000){
-        return colors[5];
-    }
-    else if(value < 7000){
-        return colors[6];
-    }
-    else if(value < 8000){
-        return colors[7];
-    }
-    else if(value < 9000){
-        return colors[8];
-    }
-    else if(value < 10000){
-        return colors[9];
+    else if(value > maxVal[plot2_type]){
+        return colors[10];
     }
     else{
-        return colors[10];
+        var index = Math.floor(value * 10 / maxVal[plot2_type]);
+        return colors[index];
     }
 }
 
@@ -68,6 +40,9 @@ var path = d3.geoPath();
 
     var svg = d3.select("#plot2-holder")
     .append("svg")
+    .call(d3.zoom().on("zoom", function () {
+        svg.attr("transform", d3.event.transform)
+    }))
     .attr("width", width)
     .attr("height", height)
     .append('g')
@@ -87,11 +62,44 @@ queue()
     .defer(d3.tsv, "data/MODIS/statistics.tsv")
     .await(ready);
 
-function ready(error, data, population) {
+// update currently created plot with new data
+function replacePlot2Content(type){
+    plot2_type = type;
+
+    d3.select("svg").remove();
+
+    svg = d3.select("#plot2-holder")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append('g')
+        .attr('class', 'map');
+
+    queue()
+        .defer(d3.json, "data/world_countries.json")
+        .defer(d3.tsv, "data/MODIS/statistics.tsv")
+        .await(ready);
+}
+
+function ready(error, data, statistics) {
     var populationById = {};
 
-    population.forEach(function(d) { populationById[d.id] = +d.occurrences; });
-    data.features.forEach(function(d) { d.population = populationById[d.id] });
+    statistics.forEach(function(d) {
+        if(plot2_type === "area_occurrences"){
+            populationById[d.id] = d.area_occurrences;
+        }
+        else if(plot2_type === "bright_ti4_high"){
+            populationById[d.id] = d.bright_ti4_high;
+        }
+        else if(plot2_type === "bright_ti4_avg"){
+            populationById[d.id] = d.bright_ti4_avg;
+        }
+        else{
+            populationById[d.id] = d.total_occurrences;
+        }
+
+    });
+    data.features.forEach(function(d) { d.value = populationById[d.id] });
 
     svg.append("g")
         .attr("class", "countries")
@@ -100,7 +108,6 @@ function ready(error, data, population) {
         .enter().append("path")
         .attr("d", path)
         .style("fill", function(d) { return customColor(populationById[d.id]); })
-        //.style("fill", customColor(populationById[d.id]))
         .style('stroke', 'white')
         .style('stroke-width', 1.5)
         .style("opacity",0.8)
@@ -124,9 +131,8 @@ function ready(error, data, population) {
                 .style("stroke-width",0.3);
         });
 
-    svg.append("path")
+    /*svg.append("path")
         .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-        // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
         .attr("class", "names")
-        .attr("d", path);
+        .attr("d", path);*/
 }
--- parse.py
+++ parse.py
@@ -2,6 +2,8 @@ import os
 import glob
 import sys
 
+# ti4 - modis
+
 satelites = ['MODIS', 'VIIRS']
 
 # get country:code dict
@@ -18,16 +20,18 @@ with open('data/world_population.tsv', 'r') as my_file:
 
 		country_code[values[1]] = values[0]
 
+# country area
+country_area = {'Cyprus': 9251, 'Armenia': 29743, 'Russia': 17098242, 'Ukraine': 603628, 'France': 643801, 'Spain': 505992, 'Sweden': 449964, 'Norway': 385178, 'Germany': 357168, 'Finland': 338145, 'Poland': 312685, 'Italy': 301318, 'United Kingdom': 244820, 'Romania': 238392, 'Belarus': 207600, 'Kazakhstan': 2724902, 'Greece': 131940, 'Bulgaria': 110994, 'Iceland': 102775, 'Hungary': 93030, 'Portugal': 91568, 'Austria': 83858, 'Czech Republic': 78866, 'Serbia': 77453, 'Ireland': 70273, 'Lithuania': 65300, 'Latvia': 64589, 'Croatia': 56594, 'Bosnia and Herzegovina': 51129, 'Slovakia': 49036, 'Estonia': 45339, 'Denmark': 44493, 'Switzerland': 41290, 'Netherlands': 41198, 'Moldova': 33846, 'Belgium': 30510, 'Albania': 28748, 'Macedonia': 25713, 'Turkey': 783562, 'Slovenia': 20273, 'Montenegro': 13812, 'Kosovo': 10908, 'Azerbaijan': 86600, 'Luxembourg': 2586, 'Georgia': 69700, 'Andorra': 468, 'Malta': 316, 'Liechtenstein': 160, 'San Marino': 61, 'Monaco': 1.95, 'Vatican_City': 0.44}
 
 for sat in satelites:
 	# open file to write
 	with open('data/'+sat+'/statistics.tsv', 'w') as the_file_elem:
-		the_file_elem.write('id\tname\toccurrences\n')
-	with open('data/'+sat+'/statistics_ti4.csv', 'w') as the_file_ti4:
-		the_file_ti4.write('country,occurrences,bright_ti4_low,bright_ti4_avg,bright_ti4_high,latitude,longitude,bright_ti4,scan,track,acq_date,acq_time,satellite,instrument,confidence,version,bright_ti5,frp,daynight\n')
+		the_file_elem.write('id\tname\ttotal_occurrences\tarea_occurrences\tbright_ti4_high\tbright_ti4_avg\n')
+	#with open('data/'+sat+'/statistics_ti4.csv', 'w') as the_file_ti4:
+	#	the_file_ti4.write('country,occurrences,bright_ti4_low,bright_ti4_avg,bright_ti4_high,latitude,longitude,bright_ti4,scan,track,acq_date,acq_time,satellite,instrument,confidence,version,bright_ti5,frp,daynight\n')
 
-	with open('data/'+sat+'/statistics_ti5.csv', 'w') as the_file_ti5:
-		the_file_ti5.write('country,occurrences,bright_ti5_low,bright_ti5_avg,bright_ti5_high,latitude,longitude,bright_ti4,scan,track,acq_date,acq_time,satellite,instrument,confidence,version,bright_ti5,frp,daynight\n')
+	#with open('data/'+sat+'/statistics_ti5.csv', 'w') as the_file_ti5:
+	#	the_file_ti5.write('country,occurrences,bright_ti5_low,bright_ti5_avg,bright_ti5_high,latitude,longitude,bright_ti4,scan,track,acq_date,acq_time,satellite,instrument,confidence,version,bright_ti5,frp,daynight\n')
 
 	for country in next(os.walk('data/'+sat))[1]:
 		# number of elements
@@ -100,13 +104,13 @@ for sat in satelites:
 
 		with open('data/'+sat+'/statistics.tsv', 'a') as the_file_elem:
 			country = country.replace('_',' ')
-			the_file_elem.write("%s\t%s\t%d\n" % (country_code[country],country,elements))
+			the_file_elem.write("%s\t%s\t%d\t%f\t%f\t%f\n" % (country_code[country], country, elements, elements/float(country_area[country]), bright_ti4_high, bright_ti4_avg))
 
-		with open('data/'+sat+'/statistics_ti4.csv', 'a') as the_file_ti4:
-			the_file_ti4.write("%s,%d,%.1f,%.2f,%.1f,%s\n" % (country, elements, bright_ti4_low, bright_ti4_avg, bright_ti4_high, bright_ti4_high_row))
+		#with open('data/'+sat+'/statistics_ti4.csv', 'a') as the_file_ti4:
+		#	the_file_ti4.write("%s,%d,%.1f,%.2f,%.1f,%s\n" % (country, elements, bright_ti4_low, bright_ti4_avg, bright_ti4_high, bright_ti4_high_row))
 
-		with open('data/'+sat+'/statistics_ti5.csv', 'a') as the_file_ti5:
-			the_file_ti5.write("%s,%d,%.1f,%.2f,%.1f,%s\n" % (country, elements, bright_ti5_low, bright_ti5_avg, bright_ti5_high, bright_ti5_high_row))
+		#with open('data/'+sat+'/statistics_ti5.csv', 'a') as the_file_ti5:
+		#	the_file_ti5.write("%s,%d,%.1f,%.2f,%.1f,%s\n" % (country, elements, bright_ti5_low, bright_ti5_avg, bright_ti5_high, bright_ti5_high_row))
 
 		#exit()
 		#for files in os.walk('data/'+sat+'/'+country):
